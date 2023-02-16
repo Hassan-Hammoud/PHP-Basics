5326c3fccdf426a29997cb9326f1cf1b135bdee6
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-16 18:06:19 +0200

    step-3

---
+++ Basics-01/step-3.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Hassan Hammoud";
+$email = "hassanhammoud2000@gmail.com";
+echo " My name is " .$name.  " and my email is " .$email. ";";
+$height = 175;
+$width = 100;
+$area = $height * $width;
+print " the aria value is " .$area;
+
+?>
