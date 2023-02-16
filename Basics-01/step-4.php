b1c0e18781a33f79e43f5aec9658df90d17b00e2
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-16 18:31:55 +0200

    step-4

---
+++ Basics-01/step-4.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Hassan";
+$position = "tech consultant";
+$github_URL = "https://github.com/Hassan-Hammoud";
+
+echo "Hello, I'm $name, I'm a " .$position. "please check my github link $github_URL.";
+?>
