c7c853b0b1fcc11906ed3e4e6f1c86660c33e127
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-19 18:47:27 +0200

    step-5

---
+++ Basics-01/step-5.php
@@ -0,0 +1,12 @@
+<?php
+$course_name = 'PHP';
+$enrolled_students = 42;
+$price = 10.2;
+$on_discount = False;
+
+echo "course Title : $course_name
+Enrolled Students: $enrolled_students
+Course price: $price
+Course on discount:" .($on_discount? "yes":"no");
+
+?>
