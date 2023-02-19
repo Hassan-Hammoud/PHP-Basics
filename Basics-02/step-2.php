5ec45cba78679b87c898f267d169050cdfe97c99
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-19 19:41:48 +0200

    step-2

---
+++ Basics-02/step-2.php
@@ -0,0 +1,12 @@
+<?php
+function greaterFn($num){
+        if ($num > 30) {
+        echo "$num is greater than 30";
+      } else if ($num > 20) {
+        echo "$num is greater than 20";
+      } else if ($num > 10) {
+        echo "$num is greater than 10";
+      }
+}
+greaterFn(25);
+?>
