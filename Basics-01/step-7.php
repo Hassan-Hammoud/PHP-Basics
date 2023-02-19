78e1ea6c55c92637320405d2c440e572084dee77
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-19 19:09:36 +0200

    step-7

---
+++ Basics-01/step-7.php
@@ -0,0 +1,8 @@
+<?php
+$birth_year = 1990;
+$current_year = 2000;
+
+$difference = $current_year - $birth_year ;
+
+    echo $difference > 18 ?  "You can drive" : "You still a kid, go and play GTA";
+?>
